5396585198dabbcfac8ec34c620019b9ca215c83
juliianacosta <42920556+juliianacosta@users.noreply.github.com>
2018-11-11 16:39:21 -0200

    Update Notify.java

--- Notify.java
+++ Notify.java
@@ -1,4 +1,13 @@
 
 public class Notify {
-
+	String chatid;
+	int unreadCount;
+	
+	public Notify(String chatid, int unreadCount) {
+		super();
+		this.chatid = chatid;
+		this.unreadCount = unreadCount;
+	}
+	
+	
 }
