93338891abf56603945e0fc449d35b58c7282f57
juliianacosta <42920556+juliianacosta@users.noreply.github.com>
2018-11-11 16:37:58 -0200

    Update Inbox.java

--- Inbox.java
+++ Inbox.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class Inbox {
 
+	User user;
+	Msg msg;
+	ArrayList<Msg> msgs = new ArrayList<Msg>();
 	
+	public Inbox(User user, Msg msg) {
+		super();
+		this.user = user;
+		this.msg = msg;
+	}
+	
+	public void mtdInbox(Msg msg){
+		msgs.add(msg);
+	}
 }
